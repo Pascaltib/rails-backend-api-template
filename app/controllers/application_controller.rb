3977a73a291f8ae9c3f37c16190a1a6702df9127
Pascal Tiberghien <pascaltib@gmail.com>
2023-05-19 03:39:08 +0200

    Add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Pundit: allow-list approach
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   protected
 
@@ -10,5 +15,11 @@ class ApplicationController < ActionController::API
     added_attrs = %i[phone_number email password password_confirmation]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
