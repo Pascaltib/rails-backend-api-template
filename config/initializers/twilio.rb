d34a7dac93397ee2d1a8619a5f75693665bb74d1
Pascal Tiberghien <pascaltib@gmail.com>
2023-05-20 03:13:28 +0200

    linting'

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 account_sid = ENV.fetch('TWILIO_ACCOUNT_SID')
 auth_token = ENV.fetch('TWILIO_AUTH_TOKEN')
 TwilioClient = Twilio::REST::Client.new(account_sid, auth_token)
